d7f03c4f7b7a85d69d8dea249bdd652ca63364d2
JuMANocta <93329464+JuMANocta@users.noreply.github.com>
2022-10-14 13:37:10 +0200

    Update App1.java

--- App1.java
+++ App1.java
@@ -1,5 +1,5 @@
 public class App1{
     public static void main(String[] args){
-        System.out.println("retour sur jenkins trigger");
+        System.out.println("retour sur jenkins trigger hook");
     }
 }
