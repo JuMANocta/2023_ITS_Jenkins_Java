aac185c8472074b08b08b97ee87e494f01b7d77f
JuMANocta <93329464+JuMANocta@users.noreply.github.com>
2023-07-20 15:02:57 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App{
     public static void main(String[] args){
-        System.out.println("retour sur jenkins trigger titi 2 4hook");
+        System.out.println("Bonjour Jean QUINS !");
     }
 }
